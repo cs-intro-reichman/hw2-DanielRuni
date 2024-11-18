35c974cb500f1e48ad19d72904c770c8a5024181
Daniel Levy <daniel.levy04@post.runi.ac.il>
2024-11-18 22:29:37 +0200

    Collatz 2nd commit

--- Collatz.java
+++ Collatz.java
@@ -30,6 +30,6 @@ public class Collatz {
 			}
 			countSteps = 1;
 		}
-		System.out.println("Every one of the first " + N + " hailstone sequences reached 1");
+		System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 	}
 }
