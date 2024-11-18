d9b775ad59f4c998c313ed7d3155a9a1a50fb4b9
Daniel Levy <daniel.levy04@post.runi.ac.il>
2024-11-18 22:00:18 +0200

    first commit

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int denominator = 3;
+		double sum = 1;
+		int number = Integer.parseInt (args[0]);
+		number --;
+			for ( int i=0; i<number/2; i++) {
+				sum -= ((double) 1 / denominator);
+				denominator += 2;
+				sum += ((double) 1 / denominator);
+				denominator += 2;
+			}
+			if 
+			(number % 2 != 0) {
+				sum -= ((double) 1 / denominator);
+			}
+	System.out.println("pi according to java: " + Math.PI);
+	System.out.println("pi, approximated:     " + sum * 4);
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,30 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            String lettersToCheck = "AEFHILMNORSX";
+            int sw = 0;
+            int times = Integer.parseInt (args[1]);
+            word = word.toUpperCase();
+            System.out.println(word);
+            for (int i=0; i<word.length(); i++)
+            {
+             for (int k=0; k<lettersToCheck.length(); k++)
+             {
+                if (word.charAt(i) == lettersToCheck.charAt(k)) {
+                        System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
+                        sw = 1;
+                }
+             }
+             if (sw == 0) {
+                System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");
+             }
+             sw = 0;
+            }
+            System.out.println("What does it spell?");
+            for (int j=0; j<times; j++)
+            {
+                System.out.println(word + "!!!");
+            }
         }
 }
